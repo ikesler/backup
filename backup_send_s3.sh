8b802038056b6deaca7ebe68e1f58419c2553c38
Igor Kesler <i.kesler@logrus.dev>
2024-02-02 20:23:17 +0300

    cleanup

--- backup_send_s3.sh
+++ backup_send_s3.sh
@@ -5,3 +5,5 @@ backup_name = "$1"
 source_dir = "/v/backup/$backup_name/pack"
 
 aws s3 cp "$source_dir" "s3://$BACKUP_S3_BUCKET/$backup_name" --recursive --profile backup --endpoint-url "$BACKUP_S3_ENDPOINT"
+
+rm -rf "/v/backup/$backup_name"
