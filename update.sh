35a4a6da7d0e297f05b67d9291bd58913beef941
Igor Kesler <i.kesler@logrus.dev>
2024-02-02 23:12:59 +0300

    fix updater

--- update.sh
+++ update.sh
@@ -2,7 +2,9 @@
 
 git checkout .
 git pull
+chmod +x ./backup_init.sh
 chmod +x ./backup_create_sqlite.sh
+chmod +x ./backup_create_files.sh
 chmod +x ./backup_pack.sh
 chmod +x ./backup_send_s3.sh
 mkdir -p '/var/tmp/backup'
