d7dcd14504d134de03f747a0ee0c0de3493a9e11
Igor Kesler <i.kesler@logrus.dev>
2024-02-02 23:37:51 +0300

    bugfix

--- backup_create_files.sh
+++ backup_create_files.sh
@@ -6,10 +6,10 @@ set -e
 export $(grep -v '^#' .env | xargs)
 
 backup_name="$1"
-find_predicate="$2"
+source_dir="$2"
+find_predicate="$3"
 
 tmp_dir="/var/tmp/backup/$backup_name/create"
 
-cmd="find $find_predicate -exec cp --parents {} $tmp_dir \;"
-echo "$cmd"
-eval "$cmd"
+(cd ${source_dir} && find . $find_predicate -exec cp --parents {} $tmp_dir \;)
+
