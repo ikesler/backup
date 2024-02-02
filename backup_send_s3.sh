08997e12e1723845bce9993beee160fea9fc3feb
Igor Kesler <i.kesler@logrus.dev>
2024-02-02 21:30:35 +0300

    bugfix

--- backup_send_s3.sh
+++ backup_send_s3.sh
@@ -7,7 +7,7 @@ export $(grep -v '^#' .env | xargs)
 
 backup_name="$1"
 
-source_dir="/var/tmp/$backup_name/pack"
+source_dir="/var/tmp/backup/$backup_name/pack"
 
 aws s3 cp "$source_dir" "s3://$BACKUP_S3_BUCKET/$backup_name" --recursive --profile backup --endpoint-url "$BACKUP_S3_ENDPOINT"
 
