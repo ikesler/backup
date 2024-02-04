6536875e8a2cef7b1808b24a47355d533fc4765c
Igor Kesler <i.kesler@logrus.dev>
2024-02-04 17:59:03 +0300

    pg backup

---
+++ backup_create_postgres.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Load env variables from .env file
+export $(grep -v '^#' .env | xargs)
+
+backup_name="$1"
+pg_host="$2"
+pg_user="$3"
+pg_db="$4"
+
+tmp_dir="/var/tmp/backup/$backup_name/create"
+
+pg_dump -h ${pg_host} -p 5432 -U ${pg_user} ${pg_db} > "$tmp_dir/${pg_db}.sql"
