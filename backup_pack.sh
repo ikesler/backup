3d8d0ea1df8aa845f40d23ea1b037280b8cedd42
Igor Kesler <i.kesler@logrus.dev>
2024-03-02 21:02:40 +0300

    timeout

--- backup_pack.sh
+++ backup_pack.sh
@@ -12,13 +12,13 @@ mkdir -p "$target_dir"
 
 cd "$source_dir"
 
-tar -czf "$target_dir/backup.tar.gz" .
+timeout 300s tar -czf "$target_dir/backup.tar.gz" .
 
-gpg --batch --yes --passphrase-file "$BACKUP_ENCRYPTION_KEY_FILE" -c "$target_dir/backup.tar.gz"
+timeout 300s gpg --batch --yes --passphrase-file "$BACKUP_ENCRYPTION_KEY_FILE" -c "$target_dir/backup.tar.gz"
 
 cd "$target_dir"
 
-split -b 100m backup.tar.gz.gpg backup.tar.gz.gpg.
+timeout 300s split -b 100m backup.tar.gz.gpg backup.tar.gz.gpg.
 
 rm backup.tar.gz
 rm backup.tar.gz.gpg
