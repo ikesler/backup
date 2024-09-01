9ad683c13f3055ebd70fcb8b512a726a1d9de643
Igor Kesler <i.kesler@logrus.dev>
2024-09-01 20:04:53 +0300

    fix

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ git pull
 chmod +x ./backup_init.sh
 chmod +x ./backup_create_sqlite.sh
 chmod +x ./backup_create_files.sh
+chmod +x ./backup_create_dir.sh
 chmod +x ./backup_create_postgres.sh
 chmod +x ./backup_pack.sh
 chmod +x ./backup_send_s3.sh
