e80645fbdeb3a162503ac56c4bd77ef83edca893
Igor Kesler <i.kesler@logrus.dev>
2024-09-03 00:07:43 +0300

    fix

--- backup_pack.sh
+++ backup_pack.sh
@@ -13,10 +13,12 @@ mkdir -p "$target_dir"
 
 cd "$source_dir"
 
-timeout 300s tar ${tar_options} -czh -f - . | age -r "$BACKUP_AGE_KEY" > "$target_dir/backup.tar.gz.age"
+timeout 300s tar ${tar_options} -czhf "$target_dir/backup.tar.gz" .
+cat "$target_dir/backup.tar.gz" | age -r "$BACKUP_AGE_KEY" > "$target_dir/backup.tar.gz.age"
 
 cd "$target_dir"
 
 timeout 300s split -b 100m backup.tar.gz.age backup.tar.gz.age.
 
 rm backup.tar.gz.age
+rm backup.tar.gz
